11c88080d4945b8989eb1d33029d49bcc4d9103c
coringuyen <coringuyen96@gmail.com>
2016-02-24 16:48:28 -0600

    Enemy
    get the enemy to shoot

---
+++ GravityShooter/Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject Bullet;
+    float delay, Timer;
+	void Start ()
+    {
+        delay = 3.0f;
+        
+	}
+	
+	void Update ()
+    {
+        Timer += Time.deltaTime;
+        if (Timer > delay)
+        {
+            Timer = 0;
+            GameObject bullet = Instantiate(Bullet) as GameObject;
+            bullet.transform.parent = transform;
+        }
+    }
+}
---
+++ GravityShooter/Assets/Scripts/EnemyBullet.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBullet : MonoBehaviour {
+
+
+	void Start ()
+    {
+       
+	}
+	
+	void Update ()
+    {
+        transform.position += Vector3.right * Time.deltaTime * 10;
+	}
+}
