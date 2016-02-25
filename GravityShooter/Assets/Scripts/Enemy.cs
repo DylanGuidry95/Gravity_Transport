4718d90a9eab5f3331cf69010a2dd15261fd8cdf
coringuyen <coringuyen96@gmail.com>
2016-02-24 18:02:53 -0600

    Enemy
    getting it shooting

--- GravityShooter/Assets/Scripts/Enemy.cs
+++ GravityShooter/Assets/Scripts/Enemy.cs
@@ -4,20 +4,21 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 
     public GameObject Bullet;
+    public GameObject enemy;
+
     float delay, Timer;
+    Vector3 pos = new Vector3(1, 0, 0);
 	void Start ()
     {
         delay = 3.0f;
-        
 	}
 	
 	void Update ()
     {
-        Timer += Time.deltaTime;
-        if (Timer > delay)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             Timer = 0;
-            GameObject bullet = Instantiate(Bullet) as GameObject;
+            GameObject bullet = Instantiate(Bullet, enemy.transform.position, enemy.transform.rotation) as GameObject;
             bullet.transform.parent = transform;
         }
     }
