3ee8d068d4e4a2f1f3f580d1cc4e8c4dabd6af36
Eric <EricMouledoux@Gmail.com>
2016-03-02 17:36:17 -0600

    Gravity Well
    Expected result, but undesired look.

---
+++ GravityShooter/Assets/Scripts/GravityWell.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityWell : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if(other.GetComponent<Rigidbody2D>())
+        {
+            Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+            Vector3 position = transform.position;
+            Vector3 otherPos = other.transform.position;
+
+            Vector3 otherToPosition =  position - otherPos;
+            otherToPosition.Normalize();
+
+            rb.velocity += new Vector2(otherToPosition.x, otherToPosition.y) * speedModifier * Time.deltaTime;
+            rb.velocity += new Vector2(Time.deltaTime, 0);
+
+            Debug.DrawRay(otherPos, rb.velocity);
+        }
+    }
+
+    public float speedModifier = 1;
+}
