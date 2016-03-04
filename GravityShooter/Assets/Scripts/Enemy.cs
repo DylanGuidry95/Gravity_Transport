6ffc20f5409fd518c8b6983eb3deed9d56427045
coringuyen <coringuyen96@gmail.com>
2016-03-04 11:29:18 -0600

    Enemy
    working on movement

--- GravityShooter/Assets/Scripts/Enemy.cs
+++ GravityShooter/Assets/Scripts/Enemy.cs
@@ -1,29 +1,32 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[System.Serializable]
 public class Enemy : MonoBehaviour {
 
     public GameObject Bullet;
     public GameObject ball;
     public float bulletSpeed;
-    //public GameObject playerrotate;
+    public float spawnRate;
 
     float delay, timer;
 	void Start ()
     {
-        delay = 3.0f;
+        delay = 2.0f;
     }
 
 	void Update ()
     {
         if (Mathf.Abs(transform.position.y - ball.transform.position.y) < 1.0f)
         {
+            Vector3 playernormal = ball.transform.position.normalized;
+            // fire bullet
             timer += Time.deltaTime;
             if (timer > delay)
             {
                 GameObject bullet = Instantiate(Bullet) as GameObject;
                 bullet.transform.position = transform.position;
-                bullet.GetComponent<Rigidbody2D>().AddForce(transform.up * bulletSpeed);
+                bullet.GetComponent<Rigidbody2D>().velocity += new Vector2(1,0) * bulletSpeed;
                 timer = 0;
             }
         }
