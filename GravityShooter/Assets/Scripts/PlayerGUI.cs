7e5ea49f41b590c2fd197e8502b4cd6c573e664d
Chui <lordbabypanda@gmail.com>
2016-03-04 11:40:12 -0600

    GUI

---
+++ GravityShooter/Assets/Scripts/PlayerGUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGUI : MonoBehaviour
+{
+    public Sprite[] playerHealth = Resources.LoadAll<Sprite>("Textures");
+
+    void PlayerBarGUI()
+    {
+
+    }
+}
--- GravityShooter/Assets/Scripts/ScoreManager.cs
+++ GravityShooter/Assets/Scripts/ScoreManager.cs
@@ -1,18 +1,24 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class ScoreManager : MonoBehaviour
 {
-    // Dodging = 10, small enemies = 20, medium enemies = 30, large = 50, bosses = 100
-    public Text scoreText;
-    public Text bossName;
-    public Slider bossSlider;
-    public Sprite playerHealth;
-    public static int score;
-    public int scoreValue = 10;
-    public int damage = 10;
+    // Dodging = 10, 
+    // small enemies = 20, 
+    // medium enemies = 30, 
+    //large = 50, 
+    //bosses = 100
 
+    public static int score;
+    public int scoreDodge = 10;
+    public int scoreSmallEnemy = 20;
+    public int scoreMediumEnemy = 30;
+    public int scoreLargeEnemy = 50;
+    public int scoreBoss = 100;
+    public Text scoreText;
+   
     void Awake()
     {
         score = 0;
@@ -21,15 +27,15 @@ public class ScoreManager : MonoBehaviour
     void Update()
     {
         scoreText.text = "Score: " + score;
-
-        // When the enemy dies, the score will increase
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            //playerHealth.
-        }
     }
 }
 
+/// <summary>
+/// Enemy Boss GUI slider needs to be handle on enemy side
+/// </summary>
+//public Text bossName;
+//public Slider bossSlider;
+//public int damage = 10;
 //if (bossSlider.value == 0)
 //{
 //    bossSlider.value -= damage;
