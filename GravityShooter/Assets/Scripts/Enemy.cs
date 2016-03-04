59f367b0fc0c1d44544d13520219220c39296d06
coringuyen <coringuyen96@gmail.com>
2016-03-03 18:04:28 -0600

    Enemy
    shooting

--- GravityShooter/Assets/Scripts/Enemy.cs
+++ GravityShooter/Assets/Scripts/Enemy.cs
@@ -4,21 +4,28 @@ using System.Collections;
 public class Enemy : MonoBehaviour {
 
     public GameObject Bullet;
+    public GameObject ball;
+    public float bulletSpeed;
     //public GameObject playerrotate;
+
+    float delay, timer;
 	void Start ()
     {
-       
+        delay = 3.0f;
     }
-	
+
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Mathf.Abs(transform.position.y - ball.transform.position.y) < 1.0f)
         {
-            GameObject bullet = Instantiate(Bullet) as GameObject;
-            bullet.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
-            bullet.transform.rotation = Quaternion.Euler(new Vector3(bullet.transform.rotation.x, bullet.transform.rotation.y + transform.rotation.y, 0));
-            //bullet.AddForce(transform.right * 500);
+            timer += Time.deltaTime;
+            if (timer > delay)
+            {
+                GameObject bullet = Instantiate(Bullet) as GameObject;
+                bullet.transform.position = transform.position;
+                bullet.GetComponent<Rigidbody2D>().AddForce(transform.up * bulletSpeed);
+                timer = 0;
+            }
         }
-
     }
 }
