d7ce9a45368680c9ae45591eb872956cc8b68a0b
Eric <EricMouledoux@Gmail.com>
2016-03-04 11:28:58 -0600

    entity manager

---
+++ GravityShooter/Assets/Scripts/EntityManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class EntityManager : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    [System.Serializable]
+    public class Entity
+    {
+        GameObject entity;
+        float spawnRate;
+        bool canSpawn;
+    }
+
+    public List<Entity> abc = new List<Entity>();
+}
