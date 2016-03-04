2439723e8ae7d369e59f6305b23dbd4cd8438261
Chui <lordbabypanda@gmail.com>
2016-03-04 11:05:17 -0600

    GUI PC

---
+++ GravityShooter/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    // Dodging = 10, small enemies = 20, medium enemies = 30, large = 50, bosses = 100
+    public Text scoreText;
+    public Text bossName;
+    public Slider bossSlider;
+    public Sprite playerHealth;
+    public static int score;
+    public int scoreValue = 10;
+    public int damage = 10;
+
+    void Awake()
+    {
+        score = 0;
+    }
+
+    void Update()
+    {
+        scoreText.text = "Score: " + score;
+
+        // When the enemy dies, the score will increase
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //playerHealth.
+        }
+    }
+}
+
+//if (bossSlider.value == 0)
+//{
+//    bossSlider.value -= damage;
+//    bossSlider.gameObject.SetActive(false);
+//    bossName.gameObject.SetActive(false);
+//}
