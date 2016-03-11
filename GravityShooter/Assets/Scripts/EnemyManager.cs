e037e82de584ff7010de68bd8180eecb93719a7f
Tran Nguyen <coringuyen96@gmail.com>
2016-03-10 23:43:13 -0600

    Enemy
    Restart with new scripts using interface

---
+++ GravityShooter/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+interface Enemymanager
+{
+    void movement();
+    bool aim();
+}
---
+++ GravityShooter/Assets/Scripts/SmallEnemy.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SmallEnemy : MonoBehaviour, Enemymanager
+{
+    public GameObject player;
+
+    void Start ()
+    {
+        player = GameObject.Find("Player");
+        //movement();
+	}
+
+    public bool aim()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void movement()
+    {
+        Vector3 point = new Vector3(player.transform.position.x, transform.position.y, 0);
+        float hyp = (transform.position - player.transform.position).magnitude;
+        float adj = (transform.position - point).magnitude; 
+        float angle = Mathf.Cos(adj / hyp) * 180 / Mathf.PI;
+        
+        if(player.transform.position.y > transform.position.y)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90 - angle));
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90 + angle));
+        }
+
+        if (Mathf.Abs(player.transform.position.y - transform.position.y) < 1)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
+        }
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        movement();
+	}
+}
