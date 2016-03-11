1a0b46280342c1cf1cd4457d6a8357bec4ff722b
Eric <EricMouledoux@Gmail.com>
2016-03-11 16:52:11 -0600

    removed start

--- GravityShooter/Assets/Scripts/Projectile.cs
+++ GravityShooter/Assets/Scripts/Projectile.cs
@@ -4,11 +4,4 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour
 {
-    void Start()
-    {
-        GetComponent<Rigidbody2D>().velocity = dir;
-    }
-
-    public Vector3 dir;
-    public float m_damage = 1;
 }
