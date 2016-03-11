2a9ed6684c094bc6dc5312cb8e8ac744a80e6ce1
Eric <EricMouledoux@Gmail.com>
2016-03-11 11:54:19 -0600

    gravity rails
    well now works on a rail system instead of "real" gravity

--- GravityShooter/Assets/Scripts/GravityWell.cs
+++ GravityShooter/Assets/Scripts/GravityWell.cs
@@ -8,17 +8,38 @@ public class GravityWell : MonoBehaviour
 {
     void Update()
     {
-        foreach(GravityObject go in m_gravObjects)
+        foreach(GravityObject g in m_gravObjects)
         {
-            switch(go.state)
+            float speed = (Time.deltaTime * m_speedModifier);
+            switch (g.state)
             {
                 case GRAV.INIT:
+                    g.state = GRAV.ENTER;
                     break;
                 case GRAV.ENTER:
+                    if (Vector3.Distance(g.entity.transform.localPosition, g.thres) > 0.1f)
+                    {
+                        g.entity.transform.localPosition += (g.thres - g.entry) * g.mag * speed;
+                    }
+                    else
+                    {
+                        g.state = GRAV.THRESHOLD;
+                    }
                     break;
                 case GRAV.THRESHOLD:
+                    if (Vector3.Distance(g.entity.transform.localPosition, g.brake) > 0.1f)
+                    {
+                        g.entity.transform.RotateAround(transform.localPosition,
+                            Vector3.forward * (g.entry.y / Mathf.Abs(g.entry.y)), g.mag * speed * 100);
+                    }
+                    else
+                    {
+                        g.state = GRAV.BROKEN;
+                    }
                     break;
                 case GRAV.BROKEN:
+                    g.rb.velocity = g.direction * -2;
+                    g.state = GRAV.END;
                     break;
                 case GRAV.END:
                     break;  
@@ -28,12 +49,44 @@ public class GravityWell : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        
+        for(int i = 0; i < m_gravObjects.Count; ++i)
+        {
+            if (m_gravObjects[i].entity == other.gameObject)
+                return;
+        }
+       
+        if(m_gravObjects.Count < m_gravMax)
+        {
+            GravityObject g = new GravityObject();
+            m_gravObjects.Add(g);
+
+            g.rb = other.GetComponent<Rigidbody2D>();
+            g.direction = g.rb.velocity;
+            g.mag = g.rb.velocity.magnitude;
+            g.rb.velocity = Vector3.zero;
+
+            g.entity = other.gameObject;
+            g.entity.transform.parent = transform;
+
+            g.entry = g.entity.transform.localPosition;
+
+            g.thres = new Vector3(g.entry.x + (0 - g.entry.x), g.entry.y, 0);
+            g.brake = new Vector3(g.thres.x, g.thres.y, 0) * -1;
+
+            g.state = GRAV.INIT;
+        }
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
-        
+        for (int i = 0; i < m_gravObjects.Count; ++i)
+        {
+            if (m_gravObjects[i].entity == other.gameObject)
+            {
+                m_gravObjects.Remove(m_gravObjects[i]);
+                other.transform.parent = null;
+            }
+        }
     }
 
     public float m_speedModifier = 1;
@@ -45,10 +98,15 @@ public class GravityWell : MonoBehaviour
     public class GravityObject
     {
         public GameObject entity;
-        public Vector2 entry;
-        public Vector2 thres;
-        public Vector2 broke;
-        public Vector2 exit;
+        public Rigidbody2D rb;
+
+        public Vector3 direction;
+        public float mag;
+
+        public Vector3 entry;
+        public Vector3 thres;
+        public Vector3 brake;
+
         public GRAV state;
     }
 
