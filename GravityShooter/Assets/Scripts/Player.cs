d9511cbe37f211d0af8f9c38fc693ea0e298e57b
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2016-03-24 09:39:11 -0500

    Revert "I want a build today"
    This reverts commit 66577445b8cedcb26589eab617fb39d051c5a36d.

--- GravityShooter/Assets/Scripts/Player.cs
+++ GravityShooter/Assets/Scripts/Player.cs
@@ -75,7 +75,7 @@ public class Player : MonoBehaviour
     private float buttonDownTime; //Used to move the player faster of slower depending on the time between key pressed and key up
 
 
-    public PlayerGUI playerUI;
+    private PlayerGUI playerUI;
 
     private bool atTop;
     private bool atBot;
