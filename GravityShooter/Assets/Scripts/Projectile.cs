d58b2383e06d14c77adf33ddb4ff652e015a4d2d
Eric <EricMouledoux@Gmail.com>
2016-03-28 14:25:31 -0500

    bullets now have trigger colliders

--- GravityShooter/Assets/Scripts/Projectile.cs
+++ GravityShooter/Assets/Scripts/Projectile.cs
@@ -10,6 +10,17 @@ public class Projectile : MonoBehaviour
         isEnemy = true;
     }
 
+    void Update()
+    {
+        if(transform.position.x < ScreenBorders.m_bottomLeft.x - 10 ||
+           transform.position.y < ScreenBorders.m_bottomLeft.y - 10 ||
+           transform.position.x > ScreenBorders.m_topRight.x   + 10 ||
+           transform.position.y > ScreenBorders.m_topRight.y   + 10)
+        {
+            Destroy(gameObject);
+        }
+     }
+
     public int damage;
     public float speed;
     public bool isEnemy;
