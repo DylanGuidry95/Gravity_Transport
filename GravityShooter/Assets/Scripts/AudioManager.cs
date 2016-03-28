b0c11e7a2fa59761ef6390072361351ac14ea1a4
Eric <EricMouledoux@Gmail.com>
2016-03-28 11:54:04 -0500

    added audio files and manager script

---
+++ GravityShooter/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+    private IEnumerator CreateAndPlayAudio(AudioClip a_clip)
+    {
+        AudioSource source = gameObject.AddComponent<AudioSource>();
+        source.clip = a_clip;
+        source.Play();
+
+        while(source.isPlaying)
+        {
+            yield return null;
+        }
+
+        Destroy(source);
+    }
+
+    public void PlayLaserAudio()
+    {
+        if(Laser)
+            StartCoroutine(CreateAndPlayAudio(Laser));
+    }
+
+    public void PlayExplodeAudio()
+    {
+        if (Explode)
+            StartCoroutine(CreateAndPlayAudio(Explode));
+    }
+
+    public AudioClip Laser;
+    public AudioClip Explode;
+}
