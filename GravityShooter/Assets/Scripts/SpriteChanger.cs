107602f1769e5d69bafb0059f7d4038e46f14c9a
Chui <lordbabypanda@gmail.com>
2016-04-13 18:03:36 -0500

    Trying to get button animated.

---
+++ GravityShooter/Assets/Scripts/SpriteChanger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SpriteChanger : MonoBehaviour
+{
+    public Image imageRenderer;
+    public Sprite[] sprites;
+    int i = 0;
+    /// <summary>
+    /// Objective - Change sprites when mouse is highlighted/hovered on 
+    /// button at runtime.
+    /// </summary>
+    public void ChangeSprites()
+    {
+        ++i;
+        imageRenderer.sprite = sprites[i];
+    }
+
+    public void OnMouseOver()
+    {
+        ChangeSprites();
+    }
+}
