01d4fab04432950bdc3b92619d69eb648e404874
Paul Chu <lordbabypanda@gmail.com>
2016-04-14 07:52:51 -0500

    Trying to get onMouseOver to work, collider isn't detecting.

--- GravityShooter/Assets/Scripts/SpriteChanger.cs
+++ GravityShooter/Assets/Scripts/SpriteChanger.cs
@@ -6,19 +6,34 @@ public class SpriteChanger : MonoBehaviour
 {
     public Image imageRenderer;
     public Sprite[] sprites;
-    int i = 0;
+    int i;
+
+    void OnMouseOver()
+    {
+        i++;
+        imageRenderer.sprite = sprites[i];
+        print("Change sprites");
+    }
+
+    void OnMouseExit()
+    {
+        imageRenderer.sprite = sprites[0];
+    }
+
     /// <summary>
     /// Objective - Change sprites when mouse is highlighted/hovered on 
     /// button at runtime.
     /// </summary>
-    public void ChangeSprites()
-    {
-        ++i;
-        imageRenderer.sprite = sprites[i];
-    }
-
-    public void OnMouseOver()
-    {
-        ChangeSprites();
-    }
+    //public void ChangeSprites()
+    //{
+    //    if (Input.GetKey(KeyCode.A))
+    //    {
+    //        i++;
+    //        if (i >= 6)
+    //        {
+    //            i = 0;
+    //        }
+    //        imageRenderer.sprite = sprites[i];
+    //    }
+    //}
 }
