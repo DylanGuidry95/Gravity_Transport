aa6f05a6167bc2159700d47de1168100bcad14de
Dylan Guidry <dylan.guidry@students.aie.edu.au>
2016-05-12 11:28:06 -0500

    Setting image for controls

---
+++ GravityShooter/Assets/Scripts/Controls.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Controls : MonoBehaviour
+{
+    public List<Sprite> controlImage = new List<Sprite>();
+	// Use this for initialization
+	void Start ()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+            GetComponent<UnityEngine.UI.Image>().sprite = controlImage[1];
+        else
+            GetComponent<UnityEngine.UI.Image>().sprite = controlImage[0];
+
+        Time.timeScale = 0;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            gameObject.SetActive(false);
+            Time.timeScale = 1;
+        }
+
+	}
+}
