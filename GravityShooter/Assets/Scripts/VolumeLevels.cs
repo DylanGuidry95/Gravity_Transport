4354b8172833983db1373e76f7364eb2d2df08a2
Eric <EricMouledoux@Gmail.com>
2016-06-03 11:53:17 -0500

    volume saves with playerprefs

--- GravityShooter/Assets/Scripts/VolumeLevels.cs
+++ GravityShooter/Assets/Scripts/VolumeLevels.cs
@@ -2,23 +2,37 @@
 
 public static class VolumeLevels
 {
-    private static float m_music = 1;
-    private static float m_effects = 1;
-    private static float m_master = 1;
-
     public static float Master
     {
-        get { return m_master; }
-        set { m_master = Mathf.Clamp(value, 0, 1); }
+        get
+        {
+            return PlayerPrefs.GetFloat("masterVolume");
+        }
+        set
+        {
+            PlayerPrefs.SetFloat("masterFloat", Mathf.Clamp(value, 0, 1));
+        }
     }
     public static float Music
     {
-        get { return m_music * m_master; }
-        set { m_music = Mathf.Clamp(value, 0, 1); }
+        get
+        {
+            return PlayerPrefs.GetFloat("musicVolume") * PlayerPrefs.GetFloat("masterVolume");
+        }
+        set
+        {
+            PlayerPrefs.SetFloat("musciVolume", Mathf.Clamp(value, 0, 1));
+        }
     }
     public static float Effects
     {
-        get { return m_effects * m_master; }
-        set { m_effects = Mathf.Clamp(value, 0, 1); }
+        get
+        {
+            return PlayerPrefs.GetFloat("effectsVolume") * PlayerPrefs.GetFloat("masterVolume");
+        }
+        set
+        {
+            PlayerPrefs.SetFloat("effectsVolume", Mathf.Clamp(value, 0, 1));
+        }
     }
 }
